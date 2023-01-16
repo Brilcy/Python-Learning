c62ab5357d3206d740300b8d2c9ec7b0c84089b2
Brilcy <122347663+Brilcy@users.noreply.github.com>
2023-01-16 04:50:44 -0800

    Create Even or odd.py

---
+++ Even or odd.py
@@ -0,0 +1,16 @@
+# 4. Write a program to print if a given number is even or odd.
+#    Example -
+#    Input : 5
+#    Output : Odd
+#    Input : 8
+#    Output : Even
+
+
+def even_or_odd(ip):
+    if ip % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+even_or_odd(86)
