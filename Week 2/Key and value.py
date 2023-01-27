0f067e575733fc673e20142f9c2c4d8c4e18eab3
Brilcy <122347663+Brilcy@users.noreply.github.com>
2023-01-27 14:08:28 -0800

    Update Key and value.py

--- Week 2/Key and value.py
+++ Week 2/Key and value.py
@@ -8,7 +8,13 @@ def key_and_value(dictionary):
     # dictionary = {"name": "Alex", "dob": "19-10-1980", "zip": 560076}
     for i in dictionary:
         print("key = " + i)
-        print("value = ", dictionary.get(i))
+        print("value = ", dictionary[i])     
 
 
 key_and_value({"name": "Alex", "dob": "19-10-1980", "zip": 560076})
+
+
+
+# dictionary.items() - gives key, values
+# dictionary.values() - gives all the values
+# dictiobary.get(key, return_value) - if the key is not in the dictionary it takes value as return_value
